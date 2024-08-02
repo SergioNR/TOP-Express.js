4a36a372d9566d24cd027a9a7db1bb9706442142
Sergio Navarro <s.navarroredondo@gmail.com>
2024-08-02 11:41:11 +0200

    ADD logic to test HTML file response

--- app.mjs
+++ app.mjs
@@ -9,7 +9,15 @@ app.get("/", (req, res) => {
     res.send("Hello, world!")
 });
 
-console.log(`this is working`)
+app.get("/fileforSendFileFunction", (req, res) => {
+    console.log(req.url);
+
+    res.status(200).sendFile("fileforSendFileFunction.html", 
+        { 
+            root: `.` 
+        });
+
+});
 
 
 app.listen(process.env.PORTEXPRESS, process.env.HOSTNAME, () => console.log(`My first Express app - listening on port ${process.env.PORTEXPRESS}!`));
